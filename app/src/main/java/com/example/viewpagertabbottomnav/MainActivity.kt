2b5b2703adf95926d1ef8a6cfc33c3d7862b5a57
emdadul_islam <emdadulislam439@gmail.com>
2021-06-18 01:36:02 +0600

    initial commit

---
+++ app/src/main/java/com/example/viewpagertabbottomnav/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.viewpagertabbottomnav
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.fragment_container_view, CollectionDemoFragment::class.java, null)
+                .commit()
+        }
+    }
+}
